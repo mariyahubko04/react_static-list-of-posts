e0102f3fb1114338a19956adfa7e5c62dc6e3acb
unknown <mariia>
2019-07-03 23:53:37 +0300

    Updates

---
+++ precache-manifest.c619e6ef85e4ec51d3158ffc1cbdc690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563854db4afa837d61aa22712d5c5479",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "de91563647e77d345178",
+    "url": "/react_static-list-of-posts/static/css/main.b4d21b2f.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "de91563647e77d345178",
+    "url": "/react_static-list-of-posts/static/js/main.d67ccb92.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
